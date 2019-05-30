6471f156a856d2e688bc97242521211fc2bb96bf
natemoo-re <ntmoore@knox.edu>
2019-05-29 22:21:13 -0500

    Initial commit

---
+++ packages/package-a/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Thing } from 'package-b';
+
+// Delete me
+export const Stuff = () => {
+  return <div>
+    Package A rendering!
+    <Thing />
+  </div>;
+};
